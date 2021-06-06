e4350edf206c6a0894134ece9adef2eb323778a7
aiyuechuang <1432803776@qq.com>
2021-06-06 09:37:28 +0800

    update

--- aiyc_code_push.sh
+++ aiyc_code_push.sh
@@ -1,7 +1,12 @@
-git pull
 git status
 git add .
 git commit . -m "update"
 #git status
+git push
+git remote add upstream https://github.com/AndersonHJB/Student_homework.git
+git fetch upstream
+git checkout main
+git merge --no-edit upstream/main
+# git commit
 git push
 
