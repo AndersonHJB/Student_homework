75a94deb72b2813d7618d7a71fd54854fbb92e5f
aiyuechuang <1432803776@qq.com>
2021-06-09 11:11:00 +0800

    update

---
+++ xiaokai/课堂代码/02-class_coder/game.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/9 11:05 上午
+# @Author  : AI悦创
+# @FileName: game.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+from text_game import Creature
