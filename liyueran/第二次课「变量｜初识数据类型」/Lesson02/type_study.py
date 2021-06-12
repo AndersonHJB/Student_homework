f18793a34e2dd3c4b632c5de13bb3ad040c2afed
aiyuechuang <1432803776@qq.com>
2021-06-12 14:40:32 +0800

    update

---
+++ liyueran/第二次课「变量｜初识数据类型」/Lesson02/type_study.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/12 2:21 下午
+# @Author  : AI悦创
+# @FileName: type_study.py.py
+# @Software: PyCharm
+# @Blog    ：http://www.aiyc.top
+# @公众号   ：AI悦创
+a = 1
+type_s = type(a)
+print(type_s)
