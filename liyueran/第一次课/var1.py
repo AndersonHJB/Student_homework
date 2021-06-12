4502ee92b9ac7b68d4a09546a869fb5f5ef6b71e
jess_lyr <jl20020510@gmail.com>
2021-06-12 14:11:05 +0800

    update

---
+++ liyueran/第一次课/var1.py
@@ -0,0 +1,14 @@
+# x = 1
+# x = x + 10
+# print(x)
+
+name1 = 'lilei'
+name2 = name1
+print("这是name2的value：", name2)
+
+name1 = 'lilei'
+name1 = 'hanmeimei'
+print(name1)
+
+
+
---
+++ liyueran/第一次课/var2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/12 1:52 PM
+# @Author  : 李悦然
+# @FileName: var2.py
+
+
+# a = b = c = 10
+#
+# a, b, c = 1, 2, 3
+# print(a)
+# print(b)
+# print(c)
+#
+# a=100
+# A=10
+# print(a)
+
+print(help("keyword"))
