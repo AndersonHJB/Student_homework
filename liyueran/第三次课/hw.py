09bca8d049ab2271c979e09848c62da161e93426
jess_lyr <jl20020510@gmail.com>
2021-06-14 23:26:50 +0800

    update

---
+++ liyueran/第三次课/hw.py
@@ -0,0 +1,10 @@
+name_input = input("Please input input name:>>>")
+gender_input = input("please input your gender:>>>")
+age_input = input("please input your age:>>>")
+school_input = input("please input your school:>>>")
+
+print("    Resume    ")
+print("Name:" + name_input)
+print("Gender:" + gender_input)
+print("Age:" + age_input)
+print("School:" + school_input)s
