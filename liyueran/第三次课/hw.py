1cd58e1eea732407f56e53122d175569b4d7fd58
aiyuechuang <1432803776@qq.com>
2021-06-16 22:11:21 +0800

    update

--- liyueran/第三次课/hw.py
+++ liyueran/第三次课/hw.py
@@ -1,10 +1,14 @@
+# ---------- input area ----------
 name_input = input("Please input input name:>>>")
+# username/user_name/username_input
 gender_input = input("please input your gender:>>>")
 age_input = input("please input your age:>>>")
 school_input = input("please input your school:>>>")
 
+# ---------- print area ----------
 print("    Resume    ")
 print("Name:" + name_input)
 print("Gender:" + gender_input)
 print("Age:" + age_input)
-print("School:" + school_input)s
+print("School:" + school_input)
+# 代码文件/变量：见名知意
---
+++ liyueran/第四次课/README.md
@@ -0,0 +1,13 @@
+# HW and list and tuple
+
+## 1. HW
+
+- 代码文件/变量：见名知意
+
+```python
+name_input = input("Please input input name:>>>")
+# username/user_name/username_input
+```
+
+
+
