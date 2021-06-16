878b1a265aeb31b96895c297e6ae1a997b6f67a4
aiyuechuang <1432803776@qq.com>
2021-06-16 22:29:38 +0800

    update

--- liyueran/第三次课/hw.py
+++ liyueran/第三次课/hw.py
@@ -1,14 +1,27 @@
 # ---------- input area ----------
 name_input = input("Please input your name:>>>")
 # username/user_name/username_input
-gender_input = input("please input your gender:>>>")
+gender_input = int(input("please input your gender:>>>"))
 age_input = input("please input your age:>>>")
 school_input = input("please input your school:>>>")
 
 # ---------- print area ----------
-print("    Resume    ")
+print("********************")
+print("\t\tResume    ")
 print("Name:" + name_input)
-print("Gender:" + gender_input)
+print("Gender:", str(gender_input))
 print("Age:" + age_input)
 print("School:" + school_input)
 # 代码文件/变量：见名知意
+# 输入你的 age >>> 出生日期
+# if age_input.isdigit():
+# 	int(age_input)
+# else:
+# 	print("xxxxx")
+# if sex == "man":
+# 	print()
+# elif sex == "wom":
+# 	print()
+# else:
+# 	print("erro")
+# option + command + L
--- liyueran/第四次课/README.md
+++ liyueran/第四次课/README.md
@@ -9,5 +9,14 @@ name_input = input("Please input input name:>>>")
 # username/user_name/username_input
 ```
 
+- Input 得到的数据类型是？>>> string
+- `In [7]: print("\t\t\tResume")`
+- 代码格式化：option + command + L
+- **只要你需求实现了，是没有对错，只有需求以及优化。**
+
+
+
+## 2. list
+
 
 
