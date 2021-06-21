444546dea590885be2cf8d5dfb2e64771c7c5a44
aiyuechuang <1432803776@qq.com>
2021-06-21 09:14:21 +0800

    update

--- liyueran/第五次课/dict_demo01.py
+++ liyueran/第五次课/dict_demo01.py
@@ -5,3 +5,15 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+# 电话本
+# 用户名称：手机号
+# list/tuple >>> list
+username = ['aiyc', 'lilei', 'hanmeimei']
+iphone_numbers = [124, 520, 131]
+# 我要查询：aiyc 电话号码
+# name = input("Please your name:>>>")
+name = "aiyc"
+try:
+	print(iphone_numbers[username.index(name)])
+except:
+	print(f"未查询到：{name}")
