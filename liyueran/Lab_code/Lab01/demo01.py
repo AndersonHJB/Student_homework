6a9bde544d0152cf09d707adba7f64f113fce6cc
aiyuechuang <1432803776@qq.com>
2021-06-23 16:48:48 +0800

    update

--- liyueran/Lab_code/Lab01/demo01.py
+++ liyueran/Lab_code/Lab01/demo01.py
@@ -5,3 +5,12 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+def sum_digits(y):
+	sum_value = 0
+	str_y = str(y)
+	for num in str_y:
+		sum_value = sum_value + int(num)
+	return sum_value
+
+if __name__ == '__main__':
+	print(sum_digits(4224))
