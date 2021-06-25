a9857b65dbac4aeafae16ddb0215ebaf509b02bd
aiyuechuang <1432803776@qq.com>
2021-06-25 15:40:09 +0800

    update

--- liyueran/第七次课/demo3.py
+++ liyueran/第七次课/demo3.py
@@ -5,3 +5,16 @@
 # @Software: PyCharm
 # @Blog    ：http://www.aiyc.top
 # @公众号   ：AI悦创
+dog_x = cat_x = 0
+
+def dog_move():
+	global dog_x
+	dog_x += 10
+
+def cat_move():
+	global cat_x
+	cat_x += 10
+
+if __name__ == '__main__':
+	dog_move()
+	cat_move()
